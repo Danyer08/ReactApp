5d9b33eca5a595db3573a9a2be8d1a9ec1c44e8d
Danyer <danjer0826@gmail.com>
2019-08-17 23:51:30 -0400

    Modified App page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex justify-content-center">
+        <div className="d-flex align-card">
+          <div className="card">
+            <div className="card-header">
+              <h1>Hello Guest</h1>
+              <img src={logo}></img>
+            </div>
+            <div className="card-body">
+              <p>This is rendered by React DOM</p>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
